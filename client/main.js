be8498c30071bb4418620fe3efd4a0c59701a3e2
Alexander Gribanov <alexander.gribanov@dataart.com>
2017-03-31 13:24:35 +0300

    Fetch locations

--- client/main.js
+++ client/main.js
@@ -8,8 +8,17 @@ $(() => {
 
     const locationSearchInput$ = Rx.Observable.fromEvent($locationSearch, 'input')
         .map(event => event.target.value)
+        .switchMap(fetchLocations)
     
     locationSearchInput$.subscribe(data => {
         console.log(data);
     })
+
+    function fetchLocations(text) {
+        const request =
+            fetch('http://localhost:8080/locations?search=' + text)
+                .then(resp => resp.json());
+    
+        return Rx.Observable.fromPromise(request);
+    }
 });
